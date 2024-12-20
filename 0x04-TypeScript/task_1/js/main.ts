f6fb1ddb5dbacc11af12fa84e9d15e26b93252c9
matthsworld <matthsworld@gmail.com>
2024-12-20 07:51:45 +0100

    Add Directors interface extending Teacher with numberOfReports attribute

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,3 +19,29 @@ const teacher3: Teacher = {
 
 console.log(teacher3);
 
+// Define the Teacher interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // Allows additional attributes
+}
+
+// Define the Directors interface extending Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example usage of the Directors interface
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
